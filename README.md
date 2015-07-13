c10cedac3f8360b49bee20f09eb88905b4735dd5
andrej <andrej@giga.webduvet.com>
2015-07-13 23:03:30 +0100

    build scripts

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # proksy
 simple reverse proxy
+
+
+## notes
+
+### build
+use npm or gulp or both?
+
+bin or .bin
+for proxy start script with default config file de[endent on build settings
+
+dist
+where the app is compiled and than able to be shipped to server...
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+
 
 gulp.task('default', function() {
   // place code for your default task here
 });
+
+gulp.task('lint', function() {
+  return gulp.src('./lib/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('YOUR_REPORTER_HERE'));
+});
+
+gulp.task('bin', function() {
+});
+
+
